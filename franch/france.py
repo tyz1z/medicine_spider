a6319f7830e850b8f90d343ce09467048952eccc
fakeyw <fakeyw@163.com>
2019-03-20 10:59:09 +0800

    fix

---
+++ franch/france.py
@@ -0,0 +1,84 @@
+import requests
+import random
+import logging
+from httputil import get_response,return_soup,post_response
+import json
+import re
+from time import sleep
+from engine import workshop,base_task
+
+list_url = "http://agence-prd.ansm.sante.fr/php/ecodex/index.php#result"
+origin_data ={"page":1,"cherche":1,"subsid":"","nomsubs":"","affliste":0,"listeOpen":0,"lstRecherche":"denomination","radLibelle":2,"txtCaracteres":"","txtDateDebut":"","txtDateFin":"","lstEtat":0,"lstComm":0,"lstDoc":0}
+
+html = post_response(list_url,origin_data)
+soup = return_soup(html)
+
+sava_path = "list.json"
+
+def getids(html):
+    return re.findall(r".*specid=(.*?)'>",html)
+    
+def clear(html):
+    pass
+    
+def get_drugs_and_save():
+    first_html = post_response(list_url,origin_data)
+    pages = re.findall(r"</b>(.*?/.*?)</td>",first_html)[0][4:]
+    pages = int(pages)
+    idlist = []
+    idlist += getids(first_html)
+    for i in range(4):
+        sleep(1)
+        origin_data['page'] = i+2
+        html = post_response(list_url,origin_data)
+        idlist += getids(html)
+    
+    with open(sava_path,'w') as f:
+        f.write(json.dumps(idlist))
+
+prefix = "http://agence-prd.ansm.sante.fr/php/ecodex/extrait.php?specid="
+
+#for front page
+def info_front(html):
+    print(html)
+    
+def info_RCP(html):
+    pass
+    
+def info_Notice(html):
+    pass
+    
+class france_tack(base_task):
+    def __init__(self,id,*args,**kw):
+        self.id = id
+        super(france_tack,self).__init__(*args,**kw)
+        
+    def run(self):
+        html = get_response(prefix+self.id)
+        return deal_info(html)
+        
+def sleep_time():
+    return random.randint(2,5)
+        
+if __name__ == '__main__':
+    #TEST
+    html = ''
+    with open('page_model.html','r') as f:
+        html = f.read()
+    deal_info(html)
+    
+    #get_drugs_and_save()
+    #ws = workshop(1,sleep_time)
+    #with open(sava_path,'r') as f:
+    #    idlist = json.loads(f.read())
+    #    for i in idlist:
+    #        ws.add_task(france_task(i,ws))
+    #        
+    #ws.run()
+
+        
+        
+        
+        
+        
+        
---
+++ franch/india.py
@@ -0,0 +1,120 @@
+import requests
+import mongoLink
+import random
+import logging
+from httputil import get_response,return_soup
+from engine import workshop,base_task
+import re
+
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.DEBUG)
+
+origin_url = 'http://www.way2healthcare.com/way2medicine'
+front_url = 'http://www.way2healthcare.com/'
+
+
+# Formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# FileHandler
+file_handler = logging.FileHandler('result.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+# Log
+logger.info('Start')
+
+
+def get_detail_url(html):
+    detail_url_list = []
+    soup = return_soup(html)
+    sidebar_links = soup.select('.sidebar_link > li > a')
+    for link in sidebar_links:
+        detail_url_list.append(link['href'].replace(' ','%20').replace('\n',''))
+    return detail_url_list
+
+# should run like this: get_detail_url(get_response(origin_url)) which will get the url list
+
+
+def check_next_page(soup):
+    guides = soup.select('#medicineWrapper > center > div > a')
+    for guide in guides:
+        if '>' == guide.text:
+            # print('nextpage')
+            # print(front_url+guide['href'])
+            return front_url+guide['href']
+    return False
+        
+# should use url_list[6] for the test
+    
+def deal(html):
+    pattern = re.compile(r'.*?medicine\(\'(.*?)\'.*?\,\'(.*?)\'.*?\,\'(.*?)\'.*?\,\'(.*?)\'.*?\,\'(.*?)\'.*?\,\'(.*?)\'.*?\,\'(.*?)\'.*?\,\'(.*?)\'.*?\);',re.S)
+    soup = return_soup(html)
+    tables = soup.select('#no-more-tables > table > tbody > tr > .cur_poi')
+    for table in tables:
+        try:
+            result = re.findall(pattern, table['onclick'])
+            for i in result[0]:
+                i = i.replace(' ','').replace('\n','').replace('\t','').replace('\r','')
+            print(result[0])
+            #mongoLink.save(*item)
+            print(*result[0])
+        except Exception:
+            logger.error('phrase error',exc_info=True)
+            logger.info('phrase error,the origin tag is: '+str(table))
+            return False
+
+
+class india_task(base_task):
+    def __init__(self,url,*args,**kw):
+        self.url = url
+        super(india_task,self).__init__(*args,**kw)
+        
+    def run(self):
+        #print("run")
+        item =[]
+        html = get_response(self.url)
+        if not html:
+            logger.error('failed to requests: '+self.url)
+            self.ws.add_task(india_task(self.url))
+            return False
+        succeed = deal(html)
+        if succeed:
+            next = check_next_page(soup)
+            if next!=False:
+                self.ws.add_task(india_task(next),0)
+        return succeed
+        
+    def retry(self):
+        print('now dealing with '+url)
+        # print(url)
+        item =[]
+        html = get_response(url)
+        if not html:
+            logger.error('failed to requests: '+url)
+            return False
+        succeed = deal(html)
+        if succeed:
+            next = check_next_page(soup)
+            if next!=False:
+                self.ws.add_task(india_task(next),0)
+        return succeed      
+
+def sleep_time():
+    return random.randint(2,5)
+    
+def main():
+    total_urls_list = get_detail_url(get_response(origin_url))
+    ws = workshop(1,sleep_time)
+    for i in total_urls_list:
+        ws.add_task(india_task(i,ws),0)
+    #ws.check()
+    ws.set_test_mode(5)
+    ws.run()
+    
+    logger.info('these should be figure out: ' + str(ws.failed_queue))
+    logger.info('finished')
+
+if __name__ == '__main__':
+    main()
+
